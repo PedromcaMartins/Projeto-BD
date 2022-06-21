af5f1b7d213127e8d0725d45ccd19d845e791b39
bernas1305 <bernardo.santos13@gmail.com>
2022-06-21 21:24:19 +0100

    esquema acabado sem bugs :)

--- esquema.sql
+++ esquema.sql
@@ -1,22 +1,22 @@
-drop table categoria cascade constraint;
-drop table categoria_simples cascade constraint;
-drop table super_categoria cascade constraint;
-drop table tem_outra cascade constraint;
-drop table produto cascade constraint;
-drop table tem_categoria cascade constraint;
-drop table IVM cascade constraint;
-drop table ponto_de_retalho cascade constraint;
-drop table instalada_em cascade constraint;
-drop table prateleira cascade constraint;
-drop table planograma cascade constraint;
-drop table retalhista cascade constraint;
-drop table responsavel_por cascade constraint;
-drop table evento_reposicao cascade constraint;
-
 --1.BASE DE DADOS
 
 CREATE DATABASE vendingmachineDB; 
 
+
+drop table tem_outra;
+drop table tem_categoria;
+drop table instalada_em;
+drop table responsavel_por;
+drop table evento_reposicao;
+drop table planograma;
+drop table prateleira;
+drop table retalhista;
+drop table produto;
+drop table categoria_simples;
+drop table super_categoria;
+drop table categoria;
+drop table IVM;
+drop table ponto_de_retalho;
 --ESQUEMA
 
 create table categoria (
@@ -26,22 +26,20 @@ create table categoria (
 
 create table categoria_simples(
     nome_cat    varchar(255),
-    primary key(nome_cat_simples),
+    primary key(nome_cat),
     foreign key(nome_cat)references categoria(nome_cat)
 );
 
 create table super_categoria(
     nome_cat    varchar(255),
-    primary key(nome_cat_super),
+    primary key(nome_cat),
     foreign key(nome_cat)references categoria(nome_cat)
 );
 
 create  table tem_outra(
-    super_categoria varchar(255),
-    categoria   varchar(255),
-    primary key(categoria),
-    foreign key(super_categoria) references super_categoria(super_categoria), --FIXME #2
-    foreign key(categoria) references categoria(categoria)
+    nome_cat   varchar(255),
+    primary key(nome_cat),
+    foreign key(nome_cat) references super_categoria(nome_cat) --FIXME #2
 );
 
 create table produto (
@@ -75,9 +73,9 @@ create table ponto_de_retalho(
 create table instalada_em(
     num_serie   float,
     fabricante  varchar(255),
-    localidade   varchar(255), --FIXME #3
+    nome   varchar(255), --FIXME #3
     primary key(num_serie, fabricante),
-    foreign key(localidade) references ponto_de_retalho(localidade) --FIXME
+    foreign key(nome) references ponto_de_retalho(nome) --FIXME
 );
 
 create table prateleira(
@@ -87,8 +85,7 @@ create table prateleira(
     altura  decimal(2,2),
     nome_cat    varchar(255),
     primary key(nro, num_serie,fabricante),
-    foreign key(num_serie) references IVM(num_serie),
-    foreign key(fabricante) references IVM(fabricante),
+    foreign key(num_serie, fabricante) references IVM(num_serie, fabricante),
     foreign key(nome_cat) references categoria(nome_cat)
 );
 
@@ -102,9 +99,7 @@ create table planograma(
     loc     varchar(255),
     primary key (ean, nro, num_serie, fabricante),
     foreign key(ean) references produto(ean),
-    foreign key(nro) references prateleira(nro),
-    foreign key(num_serie) references prateleira(num_serie),
-    foreign key(fabricante) references prateleira(fabricante)
+    foreign key(nro, num_serie, fabricante) references prateleira(nro, num_serie, fabricante)
 );
 
 create table retalhista(
@@ -132,10 +127,7 @@ create table evento_reposicao(
     unidades    smallint,
     tin float,
     primary key(ean, nro, num_serie, fabricante, instante),
-    foreign key(ean) references planograma(ean),
-    foreign key(nro) references planograma(nro),
-    foreign key(num_serie) references planograma(num_serie),
-    foreign key(fabricante) references planograma(fabricante),
+    foreign key(ean, nro, num_serie, fabricante) references planograma(ean, nro, num_serie, fabricante),
     foreign key(tin) references retalhista(tin)
 );
 
