b16c58aa250b2403cd0d3a4c08ba71de76b405a1
bernas1305 <bernardo.santos13@gmail.com>
2022-06-24 12:08:30 +0100

    restriçoes alteradas

--- ICs.sql
+++ ICs.sql
@@ -1,18 +1,22 @@
 /*Trigger para verificar se uma categoria não pode estar contida em si própria*/
-CREATE OR REPLACE TRIGGER doesnt_contain_itself
-BEFORE INSERT ON tem_outra 
-FOR EACH ROW
+CREATE OR REPLACE FUNCTION doesnt_contain_itself_proc()
+RETURNS TRIGGER AS
+$$
 BEGIN
     IF NEW.nome_cat == NEW.nome_cat_super THEN
         RAISE EXCEPTION 'Uma categoria não pode estar contida em si própria'
     END IF;
+    RETURN NEW;
 END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER doesnt_contain_itself_trigger
+BEFORE INSERT ON tem_outra 
+FOR EACH ROW EXECUTE PROCEDURE doesnt_contain_itself_proc()
 
 /*Trigger para verificar se nº unidades do evento de reposicao
 excede o do planograma*/
-CREATE OR REPLACE TRIGGER check_units
-BEFORE INSERT ON evento_reposicao 
-FOR EACH ROW
+CREATE OR REPLACE FUNCTION check_units_proc()
 DECLARE uns NUMBER;
 BEGIN
     SELECT unidades INTO uns
@@ -21,12 +25,16 @@ BEGIN
     IF NEW.unidades > uns THEN
         RAISE EXCEPTION 'Excedeu o limite de unidades'
     END IF;
+    RETURN NEW;
 END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_units_trigger
+BEFORE INSERT ON evento_reposicao 
+FOR EACH ROW EXECUTE PROCEDURE check_units_proc()
 
 /*Trigger para verificar se se pode colocar produto na prateleira*/
-CREATE OR REPLACE TRIGGER is_valid
-BEFORE INSERT ON planograma 
-FOR EACH ROW
+CREATE OR REPLACE FUNCTION is_valid_proc()
 DECLARE cat_prod varchar(255), cat_prat NUMBER;
 BEGIN
     SELECT categoria INTO cat_prod
@@ -38,4 +46,10 @@ BEGIN
     IF cat_prat == 0 THEN
         RAISE EXCEPTION 'Não existe categoria presente na prateleira'
     END IF;
+    RETURN NEW;
 END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER is_valid_proc()
+BEFORE INSERT ON planograma 
+FOR EACH ROW EXECUTE PROCEDURE is_valid_proc()
